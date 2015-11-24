749d83e256922b153b8c1a237aea0486548dcc56
Thomas Rawyler <thomas.rawyler@tegonal.com>
2015-11-24 08:21:23 +0100

    OO: new header for openolitor

--- src/main/scala/ch/openolitor/Boot.scala
+++ src/main/scala/ch/openolitor/Boot.scala
@@ -1,11 +1,13 @@
-/*   __                          __                                          *\
-*   / /____ ___ ____  ___  ___ _/ /       OpenOlitor                          *
-*  / __/ -_) _ `/ _ \/ _ \/ _ `/ /        contributed by tegonal              *
-*  \__/\__/\_, /\___/_//_/\_,_/_/         http://openolitor.ch                *
-*         /___/                                                               *
+/*                                                                           *\
+*    ____                   ____  ___ __                                      *
+*   / __ \____  ___  ____  / __ \/ (_) /_____  _____                          *
+*  / / / / __ \/ _ \/ __ \/ / / / / / __/ __ \/ ___/   OpenOlitor             *
+* / /_/ / /_/ /  __/ / / / /_/ / / / /_/ /_/ / /       contributed by tegonal *
+* \____/ .___/\___/_/ /_/\____/_/_/\__/\____/_/        http://openolitor.ch   *
+*     /_/                                                                     *
 *                                                                             *
 * This program is free software: you can redistribute it and/or modify it     *
-* under the terms of the GNU General Public License as published by    *
+* under the terms of the GNU General Public License as published by           *
 * the Free Software Foundation, either version 3 of the License,              *
 * or (at your option) any later version.                                      *
 *                                                                             *
