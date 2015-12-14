df1258e94dea929bec2dc0cbdaaf9903f4cde815
Thomas Rawyler <thomas.rawyler@tegonal.com>
2015-12-14 11:44:10 +0100

    OO-36: Abos list case class

---
+++ src/main/scala/ch/openolitor/stammdaten/models/AbosModels.scala
@@ -0,0 +1,40 @@
+/*                                                                           *\
+*    ____                   ____  ___ __                                      *
+*   / __ \____  ___  ____  / __ \/ (_) /_____  _____                          *
+*  / / / / __ \/ _ \/ __ \/ / / / / / __/ __ \/ ___/   OpenOlitor             *
+* / /_/ / /_/ /  __/ / / / /_/ / / / /_/ /_/ / /       contributed by tegonal *
+* \____/ .___/\___/_/ /_/\____/_/_/\__/\____/_/        http://openolitor.ch   *
+*     /_/                                                                     *
+*                                                                             *
+* This program is free software: you can redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by           *
+* the Free Software Foundation, either version 3 of the License,              *
+* or (at your option) any later version.                                      *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but         *
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  *
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for *
+* more details.                                                               *
+*                                                                             *
+* You should have received a copy of the GNU General Public License along     *
+* with this program. If not, see http://www.gnu.org/licenses/                 *
+*                                                                             *
+\*                                                                           */
+package ch.openolitor.stammdaten.models
+
+import ch.openolitor.stammdaten._
+import ch.openolitor.core.models._
+import java.util.UUID
+
+@SerialVersionUID(111111)
+case class AboId(id: UUID) extends BaseId
+
+@SerialVersionUID(111111)
+case class Abo(id: AboId,
+               personId: PersonId,
+               personName: String,
+               personVorname: String,
+               abotypId: AbotypId,
+               abotypName: String,
+               depotId: DepotId,
+               depotName: String) extends BaseEntity[AboId]
