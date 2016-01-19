a0118e646d3037351e0d38a2606e7abe69c3988d
toggm <mike.toggweiler@tegonal.com>
2016-01-19 21:54:56 +0100

    OO-56: migrated to akka 2.4.1

--- project/Build.scala
+++ project/Build.scala
@@ -2,7 +2,7 @@ import sbt._
 import Keys._
 
 object BuildSettings {
-  val akkaV = "2.3.14"
+  val akkaV = "2.4.1"
   val sprayV = "1.3.3"
   val scalalikeV = "2.3.0"
   
@@ -25,7 +25,8 @@ object BuildSettings {
 	    "io.spray" 			  %%  "spray-json"    					 % "1.3.2",
 	    "com.wandoulabs.akka" %%  "spray-websocket" 				 % "0.1.4",
 	    "com.typesafe.akka"   %%  "akka-actor"    					 % akkaV,
-	    "com.typesafe.akka"   %%  "akka-persistence-experimental"    % akkaV,    
+	    "com.typesafe.akka"   %%  "akka-persistence"    % akkaV,
+	    "com.typesafe.akka" %% "akka-persistence-query-experimental" % akkaV,
 	    "com.typesafe.akka"   %%  "akka-slf4j"    					 % akkaV,
 	    "com.typesafe.akka"   %%  "akka-testkit"  			    	 % akkaV   % "test",    
 	    "com.github.dnvriend" %%  "akka-persistence-inmemory" 		 % "1.0.5" % "test",
@@ -34,7 +35,7 @@ object BuildSettings {
 	    //use scala logging to log outside of the actor system
 	    "com.typesafe.scala-logging" %% "scala-logging"				 % "3.1.0",
 	    //akka persistence journal driver
-	    "com.okumin" 		  %% "akka-persistence-sql-async" 		 % "0.2.1", //use older version to stay compatible with akka 1.3.x
+	    "com.okumin" 		  %% "akka-persistence-sql-async" 		 % "0.3.1", 
 	    "com.github.mauricio" %% "mysql-async" 						 % "0.2.16",
 	    //
 	    "org.scalikejdbc" 	  %% "scalikejdbc-config"				 % scalalikeV,
