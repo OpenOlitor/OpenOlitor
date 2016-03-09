70037a3932dd6fdecd0ceb85957da542308c0eef
Alwin Egger <alwin.egger@tegonal.com>
2016-03-09 14:13:54 +0100

    new StatusRoute

---
+++ src/main/scala/ch/openolitor/status/StatusRoutes.scala
@@ -0,0 +1,62 @@
+/*                                                                           *\
+*    ____                   ____  ___ __                                      *
+*   / __ \____  ___  ____  / __ \/ (_) /_____  _____                          *
+*  / / / / __ \/ _ \/ __ \/ / / / / / __/ __ \/ ___/   OpenOlitor             *
+* / /_/ / /_/ /  __/ / / / /_/ / / / /_/ /_/ / /       contributed by tegonal *
+* \____/ .___/\___/_/ /_/\____/_/_/\__/\____/_/        http://openolitor.ch   *
+*     /_/                                                                     *
+*                                                                             *
+* This program is free software: you can redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by           *
+* the Free Software Foundation, either version 3 of the License,              *
+* or (at your option) any later version.                                      *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but         *
+* WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  *
+* or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for *
+* more details.                                                               *
+*                                                                             *
+* You should have received a copy of the GNU General Public License along     *
+* with this program. If not, see http://www.gnu.org/licenses/                 *
+*                                                                             *
+\*                                                                           */
+package ch.openolitor.status
+
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import spray.http.MediaTypes._
+import spray.httpx.marshalling.ToResponseMarshallable._
+import spray.httpx.SprayJsonSupport._
+import spray.routing.Directive.pimpApply
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+import ch.openolitor.core._
+
+case class Status(buildNr: String)
+
+trait StatusRoutes extends HttpService with DefaultRouteService {
+
+  import StatusJsonProtocol._
+  
+  def getSysConfig(): SystemConfig
+
+  val statusRoute =
+    pathPrefix("status") {
+      statusRoutes()
+    }
+
+  /**
+   * Project Status routes
+   */
+  def statusRoutes(): Route =
+      path("staticInfo") {
+        get {
+          respondWithMediaType(`application/json`) {
+            complete {
+              Status(System.getenv("application_buildnr"))
+            }
+          }
+        }
+      }
+}
