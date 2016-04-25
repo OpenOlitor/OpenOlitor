89b6b989c6ad3c1052a0f82ee74fd64eaaef2a53
Thomas Rawyler <thomas.rawyler@tegonal.com>
2016-04-25 10:46:58 +0200

    OO-115: fixed buchhaltung eventstore

--- src/main/scala/ch/openolitor/core/eventsourcing/EventStoreSerializer.scala
+++ src/main/scala/ch/openolitor/core/eventsourcing/EventStoreSerializer.scala
@@ -14,17 +14,19 @@ import ch.openolitor.stammdaten.models.CustomKundentypCreate
 import ch.openolitor.core.eventsourcing.events._
 import ch.openolitor.buchhaltung.eventsourcing.BuchhaltungEventStoreSerializer
 
-class EventStoreSerializer extends StaminaAkkaSerializer(EventStoreSerializer.eventStorePersisters) with LazyLogging with StammdatenEventStoreSerializer {
-  
+class EventStoreSerializer extends StaminaAkkaSerializer(EventStoreSerializer.eventStorePersisters)
+    with LazyLogging
+    with StammdatenEventStoreSerializer
+    with BuchhaltungEventStoreSerializer {
+
   override def toBinary(obj: AnyRef): Array[Byte] = {
     logger.debug(s"EventStoreSerielizer: toBinary: $obj")
     try {
       super.toBinary(obj)
-    }          
-    catch {
-      case e:Exception => 
+    } catch {
+      case e: Exception =>
         logger.error(s"Can't persist $obj", e)
-        stammdatenPersisters.map { persister => 
+        stammdatenPersisters.map { persister =>
           if (persister.canPersist(obj)) {
             logger.warn(s"Found persister:${persister.key}")
           }
@@ -34,14 +36,16 @@ class EventStoreSerializer extends StaminaAkkaSerializer(EventStoreSerializer.ev
   }
 }
 
-object EventStoreSerializer extends EntityStoreJsonProtocol with StammdatenEventStoreSerializer with BuchhaltungEventStoreSerializer { 
-  
+object EventStoreSerializer extends EntityStoreJsonProtocol
+    with StammdatenEventStoreSerializer
+    with BuchhaltungEventStoreSerializer {
+
   val entityPersisters = Persisters(stammdatenPersisters ++ buchhaltungPersisters)
-  
-  val entityStoreInitializedPersister = persister[EntityStoreInitialized]("entity-store-initialized")  
+
+  val entityStoreInitializedPersister = persister[EntityStoreInitialized]("entity-store-initialized")
   val entityInsertEventPersister = new EntityInsertEventPersister[V1](entityPersisters)
   val entityUpdatedEventPersister = new EntityUpdatedEventPersister[V1](entityPersisters)
   val entityDeletedEventPersister = new EntityDeletedEventPersister[V1](entityPersisters)
-  
-  val eventStorePersisters = List(entityStoreInitializedPersister, entityInsertEventPersister, entityUpdatedEventPersister, entityDeletedEventPersister)  
+
+  val eventStorePersisters = List(entityStoreInitializedPersister, entityInsertEventPersister, entityUpdatedEventPersister, entityDeletedEventPersister)
 }
