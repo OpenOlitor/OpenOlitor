220fae13fc0594c054cc8351da6e518000c95edc
Mike <mike.toggweiler@tegonal.com>
2016-05-03 07:48:07 +0200

    OO-48: parse lieferungen

--- src/main/scala/ch/openolitor/stammdaten/models/ProjektModels.scala
+++ src/main/scala/ch/openolitor/stammdaten/models/ProjektModels.scala
@@ -44,8 +44,7 @@ case class Projekt(
   erstelldat: DateTime,
   ersteller: UserId,
   modifidat: DateTime,
-  modifikator: UserId
-) extends BaseEntity[ProjektId]
+  modifikator: UserId) extends BaseEntity[ProjektId]
 
 case class ProjektModify(
   bezeichnung: String,
@@ -56,8 +55,7 @@ case class ProjektModify(
   ort: Option[String],
   preiseSichtbar: Boolean = true,
   preiseEditierbar: Boolean = false,
-  waehrung: Waehrung = CHF
-) extends JSONSerializable
+  waehrung: Waehrung = CHF) extends JSONSerializable
 
 case class KundentypId(id: String)
 
@@ -70,16 +68,15 @@ trait Kundentyp {
 }
 
 case class CustomKundentyp(
-    id: CustomKundentypId,
-    override val kundentyp: KundentypId,
-    override val beschreibung: Option[String],
-    anzahlVerknuepfungen: Int,
-    //modification flags
-    erstelldat: DateTime,
-    ersteller: UserId,
-    modifidat: DateTime,
-    modifikator: UserId
-) extends BaseEntity[CustomKundentypId] with Kundentyp {
+  id: CustomKundentypId,
+  override val kundentyp: KundentypId,
+  override val beschreibung: Option[String],
+  anzahlVerknuepfungen: Int,
+  //modification flags
+  erstelldat: DateTime,
+  ersteller: UserId,
+  modifidat: DateTime,
+  modifikator: UserId) extends BaseEntity[CustomKundentypId] with Kundentyp {
   override def system = false
 }
 
