c08cf14d9914e7e142904a0da64fa0754e60378c
Alwin Egger <alwin.egger@tegonal.com>
2017-01-23 10:46:53 +0100

    Abwesenheit auf Lieferplanung war in handleKorbStatusChanged und handleAbwesenheitCreated drinnen. Bei handleAbwesenheitCreated entfernt. Neu gezählt.

--- src/main/scala/ch/openolitor/core/db/evolution/scripts/OO506_DBScripts.scala
+++ src/main/scala/ch/openolitor/core/db/evolution/scripts/OO506_DBScripts.scala
@@ -22,34 +22,20 @@
 \*                                                                           */
 package ch.openolitor.core.db.evolution.scripts
 
-object Scripts {
-  val current =
-    V1Scripts.scripts ++
-      OO205_DBScripts.scripts ++
-      OO215_DBScripts.scripts ++
-      OO219_DBScripts.scripts ++
-      OO228_DBScripts.scripts ++
-      OO219_DBScripts_FilestoreReference.scripts ++
-      OO220_DBScripts.scripts ++
-      OO297_DBScripts.scripts ++
-      OO311_DBScripts.scripts ++
-      OO314_DBScripts.scripts ++
-      OO325_DBScripts.scripts ++
-      OO326_DBScripts.scripts ++
-      OO328_DBScripts.scripts ++
-      OO327_DBScripts.scripts ++
-      OO254_DBScripts.scripts ++
-      OO152_DBScripts.scripts ++
-      OO330_DBScripts.scripts ++
-      OO337_DBScripts.scripts ++
-      OO382_DBScripts.scripts ++
-      OO106_DBScripts_Mahnungen.scripts ++
-      OO374_DBScripts.scripts ++
-      OO374_DBScripts_aktiv_to_abo.scripts ++
-      OO461_DBScripts.scripts ++
-      OO468_DBScripts.scripts ++
-      OO433_DBScripts.scripts ++
-      OO476_DBScripts.scripts ++
-      OO499_DBScripts.scripts ++
-      OO506_DBScripts.scripts
+import ch.openolitor.core.db.evolution.Script
+import com.typesafe.scalalogging.LazyLogging
+import ch.openolitor.stammdaten.StammdatenDBMappings
+import ch.openolitor.core.SystemConfig
+import scalikejdbc._
+import scala.util.Try
+import scala.util.Success
+import ch.openolitor.stammdaten.models._
+import ch.openolitor.core.db.evolution.scripts.recalculations.RecalculateAnzahlAbwesenheitenLieferung
+import ch.openolitor.core.db.evolution.scripts.recalculations.RecalculateAnzahlGeliefertLieferung
+
+object OO506_DBScripts {
+  val scripts = Seq(
+    RecalculateAnzahlAbwesenheitenLieferung.scripts,
+    RecalculateAnzahlGeliefertLieferung.scripts
+  )
 }
