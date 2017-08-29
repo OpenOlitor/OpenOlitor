448a3b533e5a7d243e2937a6a50f8a3ed3ec218a
Thomas Rawyler <thomas.rawyler@tegonal.com>
2017-08-29 11:12:22 +0200

    OO-681: reformatted some sources

--- src/main/scala/ch/openolitor/core/db/evolution/scripts/Scripts.scala
+++ src/main/scala/ch/openolitor/core/db/evolution/scripts/Scripts.scala
@@ -64,5 +64,5 @@ object Scripts {
       OO291_OO396_DBScripts.scripts ++
       OO281_DBScripts.scripts ++
       OO618_DBScripts.scripts ++
-      OO556_DBScripts.scripts      
+      OO556_DBScripts.scripts
 }
